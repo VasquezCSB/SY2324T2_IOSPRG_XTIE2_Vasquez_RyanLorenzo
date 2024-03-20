49f1c81767c1f035b4b20e4c3d9a26b3c2453fb3
Ryan Vasquez <ryanlorenzo.vasquez@benilde.edu.ph>
2024-03-21 00:10:26 +0800

    M4 Progress
    Added Spawner

--- SurvivIO_M3_VasquezRyanLorenzo/Assets/Scripts/Spawner/Spawner.cs
+++ SurvivIO_M3_VasquezRyanLorenzo/Assets/Scripts/Spawner/Spawner.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject[] ammoLoot;
-
     public GameObject[] gunLoot;
-
     public GameObject[] objectLoot;
+    public GameObject enemy;
+    public GameObject playerTransform;
+
     public int totalLootAmount = 10;
+    public int totalEnemies = 20;
     public int randomVar;
+
+    public float spawnRadius = 100f; // The radius within which enemies can spawn around the player
+    public float minDistanceFromPlayer = 10f; // The minimum distance enemies should spawn from the player
+    public float minDistanceBetweenEnemies = 10f; // The minimum distance between spawned enemies
+    public List<Transform> spawnedEnemies = new List<Transform>();
+
 
     //Coords for Spawning
     private float _randomX;
@@ -28,19 +36,44 @@ public class Spawner : MonoBehaviour
             _randomY = Random.Range(-11, 11);
             _randPos = new Vector3(_randomX, _randomY, 0);
 
-
             if (randomVar >= 0 && randomVar <= 2)
             {
                 Debug.Log("Gun");
                 Instantiate(gunLoot[Random.Range(0, ammoLoot.Length)], _randPos, Quaternion.identity);
 
-            }else if(randomVar >= 3 && randomVar <= 9)
+            }
+            else if (randomVar >= 3 && randomVar <= 9)
             {
                 Debug.Log("Ammo");
                 Instantiate(ammoLoot[Random.Range(0, ammoLoot.Length)], _randPos, Quaternion.identity);
             }
 
         }
+
+
+        for (int i = 0; i <= totalEnemies - 1; i++)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Vector2 randomDirection = Random.insideUnitCircle.normalized;
+        Vector2 spawnPosition = (Vector2)playerTransform.transform.position + randomDirection * minDistanceFromPlayer * 10;
+
+        foreach (Transform enemy in spawnedEnemies)
+        {
+            if (Vector2.Distance(spawnPosition, enemy.position) < minDistanceBetweenEnemies)
+            {
+                // Adjust spawn position if it's too close to an existing enemy
+                spawnPosition += (spawnPosition - (Vector2)enemy.position).normalized * minDistanceBetweenEnemies;
+            }
+        }
+
+        // Spawn the enemy at the calculated position
+        Transform newEnemy = Instantiate(enemy, spawnPosition, Quaternion.identity).transform;
+        spawnedEnemies.Add(newEnemy);
 
     }
 }
